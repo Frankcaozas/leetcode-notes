4c8484ab582d6c982d0569d06c6c5a048ffcc27c
Frankcaozas <1666930632@qq.com>
2022-08-28 19:31:08 +0800

    leetcode191

---
+++ src/191.位-1-的个数.ts
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=191 lang=typescript
+ *
+ * [191] 位1的个数
+ */
+
+// @lc code=start
+function hammingWeight(n: number): number {
+  let ans = 0
+  for(let i=0; i<32; i++){
+    ans += n & 1
+    n = n >> 1
+  }
+  return ans
+}
+// @lc code=end
+
