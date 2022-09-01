3ae66dadc1f3beaec300d0492c07857327889300
Frankcaozas <1666930632@qq.com>
2022-09-01 22:44:52 +0800

    leetcode387

---
+++ src/387.字符串中的第一个唯一字符.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=387 lang=typescript
+ *
+ * [387] 字符串中的第一个唯一字符
+ */
+
+// @lc code=start
+function firstUniqChar(s: string): number {
+  const position = new Map();
+    const n = s.length;
+    for (let [i, ch] of Array.from(s).entries()) {
+        if (position.has(ch)) {
+            position.set(ch, -1);
+        } else {
+            position.set(ch, i);
+        }
+    }
+    let first = n;
+    for (let pos of position.values()) {
+        if (pos !== -1 && pos < first) {
+            first = pos;
+        }
+    }
+    if (first === n) {
+        first = -1;
+    }
+    return first;
+
+  }
+// @lc code=end
+
