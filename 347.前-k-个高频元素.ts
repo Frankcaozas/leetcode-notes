15640eb2efe0e6783179d9f1f9752d02cdebddde
Frankcaozas <1666930632@qq.com>
2022-10-28 13:08:07 +0800

    leetcode347

---
+++ 347.前-k-个高频元素.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=347 lang=typescript
+ *
+ * [347] 前 K 个高频元素
+ */
+
+// @lc code=start
+function topKFrequent(nums: number[], k: number): number[] {
+  
+};
+// @lc code=end
+
